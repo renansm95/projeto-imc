85858ff03faf78233acc55c383ff90d0f31f5c91
Renan Magalhães <renan.smagalhaes@hotmail.com>
2023-06-21 20:56:26 -0300

    Projeto IMC

---
+++ index.js
@@ -0,0 +1,75 @@
+const form = document.querySelector("#formulario");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
+  if (!altura) {
+    setResultado("Altura inválida", false);
+    return;
+  }
+
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
+
+  const msg = `Seu IMC é ${imc} (${nivelImc})`;
+  setResultado(msg, true);
+});
+
+const getNivelImc = (imc) => {
+  const nivel = [
+    "Abaixo do peso",
+    "Eutrófico",
+    "Sobrepeso",
+    "Obesidade Grau 1",
+    "Obesidade Grau 2",
+    "Obesidade Grau 3",
+  ];
+
+  if (imc >= 39.9) {
+    return nivel[5];
+  } else if (imc >= 34.9) {
+    return nivel[4];
+  } else if (imc >= 29.9) {
+    return nivel[3];
+  } else if (imc >= 24.9) {
+    return nivel[2];
+  } else if (imc >= 18.5) {
+    return nivel[1];
+  } else if (imc < 18.5) {
+    return nivel[0];
+  }
+};
+
+const getImc = (peso, altura) => {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+};
+
+const criaP = () => {
+  const p = document.createElement("p");
+  return p;
+};
+
+const setResultado = (msg, isValid) => {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add("paragrafo-resultado");
+  } else {
+    p.classList.add("bad");
+  }
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+};
